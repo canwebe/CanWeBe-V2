493ad3fc5efa8997f86a0857ded98e91b7ba472f
StrikerOne65 <golamrabbani20014@gmail.com>
2022-05-15 17:44:18 +0530

    adding infosection

--- data/data.js
+++ data/data.js
@@ -1,170 +1,205 @@
 export const employeeData = [
   {
-    name: "Golam Rabbani",
-    position: "Founder & Developer",
-    info: "Rabbani is the Lead Developer and Co-Creator of this Organization, With expertise in Full-Stack Development on PWA based technology, he is also brightfully skilled in UI/UX and AI.",
-    img: "/assets/rabbani.webp",
+    name: 'Golam Rabbani',
+    position: 'Co-Founder & Developer',
+    info: 'Rabbani is the Lead Developer and Co-Creator of this Organization, who is Full-Stack Developer expert on PWA, he is also skilled in UI/UX and AI.',
+    img: '/assets/rabbani.webp',
     social: [
       {
-        name: "twitter",
-        link: "https://twitter.com/devRabbani",
+        name: 'twitter',
+        link: 'https://twitter.com/devRabbani',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/devrabbani",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/devrabbani',
       },
       {
-        name: "github",
-        link: "https://github.com/devRabbani",
+        name: 'github',
+        link: 'https://github.com/devRabbani',
       },
       {
-        name: "gmail",
-        link: "mailto:dev.rabbani@gmail.com",
+        name: 'gmail',
+        link: 'mailto:dev.rabbani@gmail.com',
       },
     ],
   },
   {
-    name: "Kaushik Das",
-    position: "Co-Founder",
-    info: "Kaushik is Marketing maniac and Resource Specialist.He helps to publicise our products and introduce clients to our organization",
-    img: "/assets/kaushik.webp",
+    name: 'Kaushik Das',
+    position: 'Co-Founder',
+    info: 'Kaushik is Marketing maniac and Resource Specialist.He helps to publicise our products and introduce clients to our organization',
+    img: '/assets/kaushik.webp',
     social: [
       {
-        name: "instagram",
-        link: "https://www.instagram.com/kaushik2201/",
+        name: 'instagram',
+        link: 'https://www.instagram.com/kaushik2201/',
       },
       {
-        name: "twitter",
-        link: "https://twitter.com/kaushikian",
+        name: 'twitter',
+        link: 'https://twitter.com/kaushikian',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/kaushik-das-a95710145/",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/kaushik-das-a95710145/',
       },
       {
-        name: "github",
-        link: "https://github.com/kaushikd207",
+        name: 'github',
+        link: 'https://github.com/kaushikd207',
       },
     ],
   },
   {
-    name: "Ganesh Kumar",
-    position: "Developer",
-    info: "Ganesh is one of our Finest React Developer, Who gets the job done with precision and on time .",
-    img: "/assets/ganesh.webp",
+    name: 'Ganesh Kumar',
+    position: 'Developer',
+    info: 'Ganesh is one of our Finest React Developer, Who gets the job done with precision and on time .',
+    img: '/assets/ganesh.webp',
     social: [
       {
-        name: "instagram",
-        link: "https://www.instagram.com/kaushik2201/",
+        name: 'instagram',
+        link: 'https://www.instagram.com/kaushik2201/',
       },
       {
-        name: "twitter",
-        link: "https://twitter.com/GaneshTiwary13",
+        name: 'twitter',
+        link: 'https://twitter.com/GaneshTiwary13',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/ganesh-kumar-a7a1441ba/",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/ganesh-kumar-a7a1441ba/',
       },
       {
-        name: "github",
-        link: "https://github.com/rohittiwari833",
+        name: 'github',
+        link: 'https://github.com/rohittiwari833',
       },
     ],
   },
   {
-    name: "Anish Tharu",
-    position: "Developer",
-    info: "Anish is an outstanding React developer,He is known to work under pressure and clear deadlines.",
-    img: "/assets/anish.webp",
+    name: 'Anish Tharu',
+    position: 'Developer',
+    info: 'Anish is an outstanding React developer,He is known to work under pressure and clear deadlines.',
+    img: '/assets/anish.webp',
     social: [
       {
-        name: "instagram",
-        link: "https://www.instagram.com/namo_anish/",
+        name: 'instagram',
+        link: 'https://www.instagram.com/namo_anish/',
       },
       {
-        name: "facebook",
-        link: "https://www.facebook.com/anish.nammochhiyatharu",
+        name: 'facebook',
+        link: 'https://www.facebook.com/anish.nammochhiyatharu',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/tharuanish",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/tharuanish',
       },
       {
-        name: "github",
-        link: "https://github.com/tharuAnish",
+        name: 'github',
+        link: 'https://github.com/tharuAnish',
       },
     ],
   },
   {
-    name: "MOHD ZAHID",
-    position: "Developer",
-    info: "Zahid is our prime React Developer.His Product Presenting skills and Vocabulary are Unmatched ",
-    img: "/assets/zahid.webp",
+    name: 'MOHD ZAHID',
+    position: 'Developer',
+    info: 'Zahid is our prime React Developer.His Product Presenting skills and Vocabulary are Unmatched ',
+    img: '/assets/zahid.webp',
     social: [
       {
-        name: "instagram",
-        link: "https://www.instagram.com/this.is.mohd_zahid/",
+        name: 'instagram',
+        link: 'https://www.instagram.com/this.is.mohd_zahid/',
       },
       {
-        name: "twitter",
-        link: "https://twitter.com/MOHD_ZAHID_1",
+        name: 'twitter',
+        link: 'https://twitter.com/MOHD_ZAHID_1',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/mohd-zahid/",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/mohd-zahid/',
       },
       {
-        name: "github",
-        link: "https://github.com/Mohd-Zahid-01",
+        name: 'github',
+        link: 'https://github.com/Mohd-Zahid-01',
       },
     ],
   },
   {
-    name: "Hirak J Kalita",
-    position: "Graphic Designer",
-    info: "Hirak is our UI/UX Expert, His Genius Work and skills are Second to None.",
-    img: "/assets/hirak.webp",
+    name: 'Hirak J Kalita',
+    position: 'Graphic Designer',
+    info: 'Hirak is our skilled Graphic Designer, His Genius Work and skills are Second to None.',
+    img: '/assets/hirak.webp',
     social: [
       {
-        name: "instagram",
-        link: "https://www.instagram.com/its_hirak_jk/",
+        name: 'instagram',
+        link: 'https://www.instagram.com/its_hirak_jk/',
       },
       {
-        name: "facebook",
-        link: "https://www.facebook.com/hirakjyoti.kalita99",
+        name: 'facebook',
+        link: 'https://www.facebook.com/hirakjyoti.kalita99',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/hirakjk99",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/hirakjk99',
       },
       {
-        name: "github",
-        link: "https://github.com/HirakJK99",
+        name: 'github',
+        link: 'https://github.com/HirakJK99',
       },
     ],
   },
   {
-    name: "Rejaul Hoque",
-    position: "Marketing Expert",
-    info: "Rejaul is ne of well commmuned marketing executives, His intellect has allowed us develop multiple products.",
-    img: "/assets/rejaul.webp",
+    name: 'Rejaul Hoque',
+    position: 'Marketing Expert',
+    info: 'Rejaul is ne of well commmuned marketing executives, His intellect has allowed us develop multiple products.',
+    img: '/assets/rejaul.webp',
     social: [
       {
-        name: "instagram",
-        link: "http://instagram.com/rejaul3301",
+        name: 'instagram',
+        link: 'http://instagram.com/rejaul3301',
       },
       {
-        name: "facebook",
-        link: "https://www.facebook.com/rambo.reja.5",
+        name: 'facebook',
+        link: 'https://www.facebook.com/rambo.reja.5',
       },
       {
-        name: "linkedin",
-        link: "https://www.linkedin.com/in/rejaul-hoque-0656041aa/",
+        name: 'linkedin',
+        link: 'https://www.linkedin.com/in/rejaul-hoque-0656041aa/',
       },
       {
-        name: "twitter",
-        link: "https://twitter.com/1046Hoque",
+        name: 'twitter',
+        link: 'https://twitter.com/1046Hoque',
       },
     ],
   },
-];
+]
+
+export const testimonyData = [
+  {
+    name: 'Daniel Clifford',
+    quoteTitle:
+      'I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.',
+    quote:
+      'I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people whom had an amazing experience here. I signed up for the free intro course and found it incredibly fun!  at a VR startup.',
+  },
+  {
+    name: 'Jonathan Walters',
+    quoteTitle: 'The team was very supportive and kept me motivated',
+    quote:
+      'I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of I’ve made in myself.',
+  },
+  {
+    name: 'Jeanette Harmon',
+    quoteTitle: 'An overall wonderful and rewarding experience',
+    quote:
+      'Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love.',
+  },
+  {
+    name: 'Patrick Abrams',
+    quoteTitle:
+      'Awesome teaching support from TAs who did the bootcamp themselves.Getting guidance from them and learning from their experiences was easy.',
+    quote:
+      'The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out You will get the personal attention you need from an incredible community of smart and amazing people.',
+  },
+  {
+    name: 'Kira Whittle',
+    quoteTitle: 'Such a life-changing experience. Highly recommended!',
+    quote:
+      'Before joining the bootcamp, I’ve never written a line of code.I needed some structure from professionals who can help me learn programming step by step. I was encouraged to enroll by a former student of theirs who can only say wonderful things about the program. The entire curriculum and staff did not disappoint. They were very hands-on and developer after receiving multiple offers.100% recommend!',
+  },
+]
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import Modal from '../components/modal'
 import ProjectScroll from '../components/projectScroller'
 import TeamList from '../components/teamList'
 import TestimonyGrid from '../components/testimonyGrid'
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+} from 'react-icons/fa'
 import { getProjectList } from '../helpers'
 import styles from '../styles/Home.module.css'
 
@@ -115,6 +122,64 @@ export default function Home({ data }) {
               >
                 Rate Us
               </a>
+            </div>
+          </div>
+        </section>
+        <section className={styles.infoSection}>
+          <div className='wrapper'>
+            <div>
+              <h2 className={styles.teamName}>CanWeBe!</h2>
+              <div className={styles.teamSocialWrapper}>
+                <a
+                  className={styles.facebook}
+                  href='https://www.facebook.com/TeamCanWeBe'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <FaFacebook />
+                </a>
+                <a
+                  className={styles.instagram}
+                  href='https://www.instagram.com/canwebeofficial/'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <FaInstagram />
+                </a>
+                <a
+                  className={styles.linkedin}
+                  href='https://www.linkedin.com/company/canwebe'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <FaLinkedin />
+                </a>
+                <a
+                  className={styles.youtube}
+                  href='https://www.youtube.com/channel/UCZQme48ejS0QY3C4JmWgI4Q'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <FaYoutube />
+                </a>
+                <a
+                  className={styles.twitter}
+                  href='https://twitter.com/teamcanwebe'
+                  target='_blank'
+                  rel='noreferrer'
+                >
+                  <FaTwitter />
+                </a>
+              </div>
+              <h4>contact@canwebe.tech</h4>
+              <p className={styles.teamPara}>
+                CanWeBe is a software development organization which is founded
+                by a group of students.We here at canwebe mainly developed
+                softwares to solve daily life problems, Other than this we have
+                a team of varius skilled person from different different field
+                of work. We collaborate and brainstorm on ideas and deliver easy
+                to use software to general users free of cost.
+              </p>
             </div>
           </div>
         </section>
